8cdf0462a16f10303a20ccbd973575e23c3e152a
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-02-23 17:11:15 +0200

    - created new component editablespan

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -47,7 +47,7 @@ export function Todolist(props: PropsType) {
 
                     return <li key={t.id} className={t.isDone ? "is-done" : ""}>
                         <input type="checkbox" onChange={onChangeHandler} checked={t.isDone}/>
-                        <span>{t.title}</span>
+                        <EditableSpan title={t.title} />
                         <button onClick={onClickHandler}>x</button>
                     </li>
                 })
@@ -67,3 +67,10 @@ export function Todolist(props: PropsType) {
     </div>
 }
 
+type EditableSpanType = {
+    title:string
+}
+
+const EditableSpan = (props: EditableSpanType)=>{
+    return <span>{props.title}</span>
+}
